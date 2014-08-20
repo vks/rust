414b31299172917f72458502d811da395f7b7b11
Michael Woerister <michaelwoerister@posteo>
2014-08-20 13:43:16 +0200

    debuginfo: Try-branch stuff...

--- mk/tests.mk
+++ mk/tests.mk
@@ -179,10 +179,7 @@ check-notidy: cleantmptestlogs cleantestlibs all check-stage2
 	$(Q)$(CFG_PYTHON) $(S)src/etc/check-summary.py tmp/*.log
 
 # A slightly smaller set of tests for smoke testing.
-check-lite: cleantestlibs cleantmptestlogs \
-	$(foreach crate,$(TEST_TARGET_CRATES),check-stage2-$(crate)) \
-	check-stage2-rpass \
-	check-stage2-rfail check-stage2-cfail check-stage2-rmake
+check-lite: cleantestlibs cleantmptestlogs check-stage1-debuginfo-gdb
 	$(Q)$(CFG_PYTHON) $(S)src/etc/check-summary.py tmp/*.log
 
 # Only check the 'reference' tests: rpass/cfail/rfail/rmake.
